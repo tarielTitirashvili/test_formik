0a48f27715d7c3bb7dfb28b5541214bc1d3c2540
tariel <tarieltitirashvili@gmail.com>
2022-09-25 12:20:22 -0700

    up to 19 lesson added callback between Field component

--- formic_app/src/components/youtubeForm.js
+++ formic_app/src/components/youtubeForm.js
@@ -6,13 +6,16 @@ const initialValues = {
   name: "",
   lastName: "",
   email: "",
+  comment: "",
+  address: ""
 };
-const onSubmit = (values) => console.log("values");
+const onSubmit = (values) => console.log(values);
 
 const validationSchema = Yup.object({
   name: Yup.string().required("Required"),
   lastName: Yup.string().required("Required").min(3) ,
   email: Yup.string().email("pleace enter valid  email address").required("Required"),
+  comment: Yup.string().required("Required")
 })
 
 export default function YoutubeForm() {
@@ -27,20 +30,43 @@ export default function YoutubeForm() {
         <Form className="form">
           <div>
             <label htmlFor="name">name</label>
-            <Field name="name" id="name" type={"text"} />
+            <Field name="name" placeholder="name" id="name" type="text" />
             <ErrorMessage name="name" />
           </div>
 
           <div>
             <label htmlFor="lastName">lastName</label>
-            <Field name="lastName" id="lastName" type={"text"} />
+            <Field name="lastName" placeholder="lastName" id="lastName" type="text" />
             <ErrorMessage name="lastName" />
           </div>
 
           <div>
             <label htmlFor="email">email</label>
-            <Field name="email" id="email" type={"email"} />
+            <Field name="email" placeholder="email" id="email" type="email" />
             <ErrorMessage name="email" />
+          </div>
+
+          <div>
+            <label htmlFor="comment">comment</label>
+            <Field as="textarea" name="comment" placeholder="comment" id="comment" type="text" />
+            <ErrorMessage name="comment" />
+          </div>
+
+          <div>
+            <label htmlFor="address">Address</label>
+            <Field name="address" placeholder="address"type="text" >
+              {
+                (props)=>{
+                  const {field, form, meta} = props
+                  console.log(props)
+                  return <div>
+                    <input id="address" {...field} />
+                    {meta.touched && meta.error && <div>{meta.error}</div>}
+                  </div>
+                }
+              }
+            </Field>
+            <ErrorMessage name="address" />
           </div>
 
           <button type="submit">submit</button>
