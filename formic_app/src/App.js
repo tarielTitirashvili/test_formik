77545f5c513b47a0c04421277ff8cfee72cfdf5d
tarielTitirashvili <tarieltitirashvili@gmail.com>
2022-09-27 17:25:36 +0400

    up to 40 lesson login form for testing

--- formic_app/src/App.js
+++ formic_app/src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import YoutubeForm from "./components/youtubeForm";
 import { NavLink, Route, Routes } from "react-router-dom";
 import UsageFormControls from "./components/afterBases/usageFormControls";
+import LoginForm from "./components/afterBases/loginForm";
 
 function App() {
   return (
     <div>
       <header>
         <div className="header">
+          <NavLink to={"/login"}>login</NavLink>
           <NavLink to={"/youtube"}>youtubeForm</NavLink>
-          <NavLink to={"/"}>login</NavLink>
+          <NavLink to={"/"}>usage testing</NavLink>
         </div>
       </header>
       <Routes>
-        <Route path="/" element={<UsageFormControls />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route path="/youtube" element={<YoutubeForm />} />
+        <Route path="/" element={<UsageFormControls />} />
       </Routes>
     </div>
   );
---
+++ formic_app/src/components/afterBases/loginForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./controls/formikControl";
+
+export default function LoginForm() {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required").email("incorrect email format"),
+    password: Yup.string().required("Required"),
+  });
+  const onSubmit = (values) => {
+    console.log("submit", values);
+  };
+  return (
+    <div className="formContainer">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => {
+          return (
+            <Form>
+              <FormikControl
+                control="input"
+                type="email"
+                name="email"
+                label="enter your email"
+              />
+              <FormikControl
+                control="input"
+                type="password"
+                name="password"
+                label="enter your password"
+              />
+              <button type="submit" disabled={!formik.isValid}>submit</button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+}
