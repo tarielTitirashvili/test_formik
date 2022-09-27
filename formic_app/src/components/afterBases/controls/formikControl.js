2990e184fbeed4f15535586098fae6111094788b
tarielTitirashvili <tarieltitirashvili@gmail.com>
2022-09-27 11:59:04 +0400

    up to 36 lesson. add textarea and select in form controls

--- formic_app/src/components/afterBases/controls/formikControl.js
+++ formic_app/src/components/afterBases/controls/formikControl.js
@@ -1,17 +1,18 @@
 import React from "react";
+import TextArea from "./textArea";
 import Input from "./input";
+import Select from "./select";
 
 export default function FormikControl(props) {
   const { control, ...rest } = props;
   switch (control) {
     case "input": return <Input {...rest}/>
-    case "textarea":
-    case "select":
+    case "textarea":return <TextArea {...rest}/>
+    case "select":return <Select {...rest}/>
     case "radio":
     case "checkbox":
     case "date":
     default:
       return null;
   }
-  return <div>FormikControl</div>;
 }
--- formic_app/src/components/afterBases/controls/textArea.js
+++ formic_app/src/components/afterBases/controls/textArea.js
@@ -1,13 +1,14 @@
 import { ErrorMessage, Field } from "formik";
 import React from "react";
-import ErrorMessageShow from "../errorMessage";
+import ErrorMessageShow from "../../errorMessage";
 
-export default function Input(props) {
+export default function TextArea(props) {
   const { label, name, ...rest } = props;
+
   return (
     <div className="form-control">
       <label htmlFor={name}>{label}</label>
-      <Field id={name} name={name} {...rest} />
+      <Field as="textarea" id={name} name={name} {...rest} />
       <ErrorMessage name={name} component={ErrorMessageShow} />
     </div>
   );
